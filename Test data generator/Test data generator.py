4d9f56829cf0430b67b43013058b08470b4ea78e
Srimonti Dutta <38216236+srimontidutta@users.noreply.github.com>
2021-04-09 17:56:42 +0530

    Add files via upload

--- Test data generator/Test data generator.py
+++ Test data generator/Test data generator.py
@@ -1,33 +1,23 @@
-#!/usr/bin/env python
-# coding: utf-8
-
-# # Importing Libraries
-
-# In[1]:
-
-
+## Importing Libraries
 from faker import Faker
 import random
 from random import randint
 import csv
-
-
-# # Data Generator Function
-
-# In[2]:
-
-
+## Data Generator Function
 def datagen(np, parameters):
+    """
+    Python function to generate test dataset of students taking an online course using Faker.
+    """
     fake = Faker()
     with open("Data_generator.csv", 'wt', newline='') as csvFile:
         writer = csv.DictWriter(csvFile, fieldnames=parameters)
-        writer.writeheader() 
+        writer.writeheader()
         for i in range(np):
             ph1=str(random.randint(0,999))
             ph2=str(random.randint(0,999)).zfill(3)
             ph3=str(random.randint(0,9999)).zfill(4)
             phone_number="{}-{}-{}".format(ph1,ph2,ph3)
-            
+
             writer.writerow({
                 "Student Name": fake.name(),
                 "Country": fake.country(),
@@ -39,24 +29,13 @@ def datagen(np, parameters):
                 "House Address": fake.address(),
                 "Latitude": fake.latitude(),
                 "Longitude": fake.longitude()
-            })          
-
-
-# # Function Call
-
-# In[3]:
-
-
+            })
+"""
+Saving the geneated dataset in a csv file on your local machine.
+"""
 if __name__ == '__main__':
     np = int(input(" Enter Number of Student Data:"))
     parameters = ["Student ID", "Student Name", "Email Id", "Phone Number", "Date of Birth", "House Address",
                "Latitude", "Longitude", "Country", "Course Comments"]
     datagen(np, parameters)
     print("File saved.")
-
-
-# In[ ]:
-
-
-
-
